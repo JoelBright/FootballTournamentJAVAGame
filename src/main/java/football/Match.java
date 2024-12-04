b8600f42c016476617381eabf247ccbcd9c68acb
JoelBright <joelbright@live.com>
2024-12-04 19:35:00 +0530

    Managed to: - update code style - fixed score calculation errors - corrected score weightage - added more players - corrected team and player names and player ages

--- src/main/java/football/Match.java
+++ src/main/java/football/Match.java
@@ -1,87 +1,121 @@
 package football;
 
 public class Match {
-    Team team1;
-    Team team2;
-    int score1;
-    int score2;
-    Team winner;
+  Team team1;
+  Team team2;
+  int score1;
+  int score2;
+  int positiveScoreActivity;
+  int negativeScoreActivity;
+  Team winner;
 
-    public Match(Team team1, Team team2) {
-        this.team1 = team1;
-        this.team2 = team2;
-        this.score1 = 0;
-        this.score2 = 0;
-        winner = null;
+  public Match(Team team1, Team team2) {
+    this.team1 = team1;
+    this.team2 = team2;
+    this.score1 = 0;
+    this.score2 = 0;
+    winner = null;
+  }
+
+  @Override
+  public String toString() {
+    String homeTeam = team1.getName();
+    String guestTeam = team2.getName();
+    StringBuilder outputString = new StringBuilder(
+        "{Match " + homeTeam + " v.s " + guestTeam + " details: { Home team:" + homeTeam + ", Guest team:" + guestTeam + ", current scores " + score1 + "-" + score2);
+
+    if ( (winner == null) ) {
+      outputString.append(", Game not yet decided}");
+    }
+    else {
+      outputString.append(", winner:").append(winner.getName()).append('}');
     }
 
-    @Override
-    public String toString() {
-        String outputString = "{Match details: " + "Home team=" + team1.getName() + ", Guest team=" + team2.getName() + ", score1=" + score1 + ", score2=" + score2 + ", winner=";
+    return outputString.toString() + '}';
+  }
 
-        outputString += (winner == null) ? "Not yet decided" : winner.getName();
+  private int getActivity(Position position, boolean overtime) {
+    int activity;
+    positiveScoreActivity = 0;
+    negativeScoreActivity = 0;
+    int max = 7;
+    int min = 1;
+    if ( overtime ) {
+      max /= 2;
+      min /= 2;
+    }
+    switch ( position ) {
+      case STRIKER -> {
+        activity = (int) (Math.random() * (max*2 - min*2 + 1) + min*2);
+        positiveScoreActivity += activity;
+      }
+      case MIDFIELDER_WM -> {
+        activity = (int) (Math.random() * (max - min + 1) + min);
+        positiveScoreActivity += activity;
+      }
+      case MIDFIELDER_CM -> {
+        activity = (int) (Math.random() * (max/2 + 1) + min);
+        positiveScoreActivity += activity;
+      }
+      case DEFENDER_CB -> {
+        activity = (int) (Math.random() * (max*2 + 1) + min);
+        negativeScoreActivity -= activity;
+      }
+      case DEFENDER_FB -> {
+        activity = (int) (Math.random() * (max - min + 1) + min);
+        negativeScoreActivity -= activity;
+      }
+      case GOALKEEPER -> {
+        activity = (int) (Math.random() * (max/2 + 1) + min);
+        negativeScoreActivity -= activity;
+      }
+      default -> throw new IllegalStateException("Unexpected value: " + position);
+    }
+    return activity;
+  }
 
-        return outputString + '}';
+  private void gameplayOccurs(boolean overtime) {
+    for ( Player playerOfTeam1 : team1.getPlayers() ) {
+      System.out.println(
+          playerOfTeam1.getPosition() + " of Home team:" + team1.getName() + " " + playerOfTeam1.getName() + " achieved an activity of: " + getActivity(
+              playerOfTeam1.getPosition(), overtime));
+    }
+    score1 += Math.abs(positiveScoreActivity);
+    score2 += Math.abs(negativeScoreActivity);
+    for ( Player playerOfTeam2 : team2.getPlayers() ) {
+      System.out.println(
+          playerOfTeam2.getPosition() + " of Guest team:" + team2.getName() + " " + playerOfTeam2.getName() + " achieved an activity of: " + getActivity(
+              playerOfTeam2.getPosition(), overtime));
+    }
+    score1 += Math.abs(negativeScoreActivity);
+    score2 += Math.abs(positiveScoreActivity);
+  }
+
+  public Team playMatch() {
+    score1 = 0;
+    score2 = 0;
+    System.out.println(
+        "Initial score: " + score1 + "-" + score2 + " Home Team:" + team1.getName() + " scored:" + score1 + " while " + "Guest Team:" + team2.getName() + " scored:" + score2);
+    gameplayOccurs(false);
+
+    System.out.println(
+        "\nHalftime break initiated!!\n" + "Current score: " + score1 + "-" + score2 + " Home Team:" + team1.getName() + " scored:" + score1 + " while " + "Guest Team:" + team2.getName() + " scored:" + score2);
+    gameplayOccurs(false);
+
+    System.out.println(
+        "\n2nd half concluded!!\n" + "Final score: " + score1 + "-" + score2 + " Home Team:" + team1.getName() + " scored:" + score1 + " while Guest Team:" + team2.getName() + " scored:" + score2);
+
+    Team matchWinner;
+    while ( score1 == score2 ) {
+      System.out.println("\nOvertime triggered:");
+      gameplayOccurs(true);
+      System.out.println("Overtime concluded!!");
     }
 
-    public Team playMatch() {
-        score1 = 0;
-        score2 = 0;
+    matchWinner = (score1 > score2) ? team1 : team2;
+    System.out.println(
+        "The match " + team1.getName() + " v.s " + team2.getName() + " was won by: " + matchWinner.getName() + "!!!\n");
 
-        for (Player playerOfTeam1 : team1.getPlayers()) {
-            int activity;
-            switch (playerOfTeam1.getPosition()) {
-                case "Striker" -> {
-                    activity = (int) (Math.random() * (20 - 10 + 1) + 10);
-                    score1 += activity;
-                }
-                case "Defender" -> {
-                    activity = (int) (Math.random() * (10 + 1) + 0);
-                    score2 -= activity;
-                }
-                case "Goalie" -> {
-                    activity = (int) (Math.random() * (5 + 1) + 0);
-                    score2 -= activity;
-                }
-                default -> throw new IllegalStateException("Unexpected value: " + playerOfTeam1.getPosition());
-            }
-            System.out.println(playerOfTeam1.getPosition() + " of Home team:" + team1.getName() + " " + playerOfTeam1.getName() + " achieved an activity of: " + activity);
-        }
-
-        for (Player playerOfTeam2 : team2.getPlayers()) {
-            int activity;
-            switch (playerOfTeam2.getPosition()) {
-                case "Striker" -> {
-                    activity = (int) (Math.random() * (20 - 10 + 1) + 10);
-                    score2 += activity;
-                }
-                case "Defender" -> {
-                    activity = (int) (Math.random() * (10 + 1) + 0);
-                    score1 -= activity;
-                }
-                case "Goalie" -> {
-                    activity = (int) (Math.random() * (5 + 1) + 0);
-                    score1 -= activity;
-                }
-                default -> throw new IllegalStateException("Unexpected value: " + playerOfTeam2.getPosition());
-            }
-            System.out.println(playerOfTeam2.getPosition() + " of Guest team:" + team2.getName() + " " + playerOfTeam2.getName() + " achieved an activity of: " + activity);
-        }
-
-        score1 = Math.abs(score1);
-        score2 = Math.abs(score2);
-
-        System.out.println("Final score: " + score1 + "-" + score2 + " Home Team:" + team1.getName() + " scored:" + score1 + " while Guest Team:" + team2.getName() + " scored:" + score2);
-
-        Team matchWinner;
-
-        if (score1 == score2) {
-            System.out.println("\nRematch scheduled and triggered:");
-            matchWinner = playMatch();
-        } else {
-            matchWinner = (score1 > score2) ? team1 : team2;
-            System.out.println("The match " + team1.getName() + " v.s " + team2.getName() + " was won by: " + matchWinner.getName() + "!!!\n");
-        }
-        return matchWinner;
-    }
+    return matchWinner;
+  }
 }
--- src/main/java/football/Team.java
+++ src/main/java/football/Team.java
@@ -3,30 +3,30 @@ package football;
 import java.util.ArrayList;
 
 public class Team {
-    String name;
-    ArrayList<Player> players;
+  String name;
+  ArrayList<Player> players;
 
-    public Team(String name) {
-        this.name = name;
-        players = new ArrayList<>();
-    }
+  public Team(String name) {
+    this.name = name;
+    players = new ArrayList<>();
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public ArrayList<Player> getPlayers() {
-        return players;
-    }
+  public ArrayList<Player> getPlayers() {
+    return players;
+  }
 
-    public void setPlayers(ArrayList<Player> players) {
-        this.players = players;
-    }
+  public void setPlayers(ArrayList<Player> players) {
+    this.players = players;
+  }
 
-    @Override
-    public String toString() {
-        StringBuilder outputString = new StringBuilder("{ Team name:" + name + ", players:");
-        for (Player player : players) outputString.append(player.toString()).append(", ");
-        return outputString.toString() + '}';
-    }
+  @Override
+  public String toString() {
+    StringBuilder outputString = new StringBuilder("{ Team name:" + name + ", players:\n");
+    for ( Player player : players ) outputString.append(player.toString()).append(",\n");
+    return outputString.toString() + '}';
+  }
 }
